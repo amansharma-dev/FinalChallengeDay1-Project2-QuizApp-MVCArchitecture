70efb5e393f97a4e2c6b2dc10b9daafac8af6ef4
AmanSharma <amansharma.11@hotmail.com>
2020-08-17 12:32:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/finalchallengeday1_project2_quizapp_mvcarchitecture/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.finalchallengeday1_project2_quizapp_mvcarchitecture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
